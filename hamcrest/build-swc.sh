480a1521d93381f3b131312203d09fbe0763d485
drewbourne <andrew@firstbourne.com>
2010-02-08 23:46:09 +1100

    bumped build-swc.sh to output version 1.1.0

--- hamcrest/build-swc.sh
+++ hamcrest/build-swc.sh
@@ -3,4 +3,4 @@ compc \
  -source-path src \
  -include-sources src \
  -namespace http://hamcrest.org/2009/mxml src/hamcrest-manifest.xml \
- -output bin/hamcrest-1.0.2.swc
+ -output bin/hamcrest-1.1.0.swc
